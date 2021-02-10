78d29fe819858f9965ece6df3d794047ad78a177
Adrian Mendez <adrianmndz328@gmail.com>
2021-02-09 23:09:35 -0600

    bug fix: handle create is now being passed the form value

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
   }
 
   const handleCreate = newProject => {
+    console.log(newProject)
     fetch(url + '/projects/', {
       method: "post",
       headers: {
@@ -43,24 +44,24 @@ function App() {
       <div className="form-container">
           <div className="header">Add a project</div>
           <div className="form">
-              <label for="name">Project Name:</label>
+              <label htmlFor="name">Project Name:</label>
               <input type="text" name="name" value={formVals.name} onChange={handleChange}/>
-              <label for="tech">Tech:</label>
+              <label htmlFor="tech">Tech:</label>
               <select name="tech" onChange={handleChange}>
                   <option value="javascript">JavaScript</option>
                   <option value="react">React</option>
                   <option value="ruby">Ruby</option>
               </select>
-              <label for="image">Image: </label>
+              <label htmlFor="image">Image: </label>
               <input type="text" name="image" onChange={handleChange}/>
-              <label for="repo">Repo: </label>
+              <label htmlFor="repo">Repo: </label>
               <input type="text" name="repo" onChange={handleChange}/>
-              <label for="demo">Live Demo: </label>
+              <label htmlFor="demo">Live Demo: </label>
               <input type="text" name="demo" onChange={handleChange}/>
-              <label for="description">Description:</label>
+              <label htmlFor="description">Description:</label>
               <textarea type="text" name="description" onChange={handleChange}></textarea>
               <div className="button">
-                  <button onClick={handleCreate}>Submit</button>
+                  <button onClick={() => handleCreate(formVals)}>Submit</button>
               </div>
           </div>
       </div>
